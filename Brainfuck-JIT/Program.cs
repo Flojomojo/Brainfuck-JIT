8944f82bfaff3d59bfa2dab8fd3aa501639167ea
Flojomojo <35773711+Flojomojo@users.noreply.github.com>
2024-01-24 21:07:03 +0100

    initial commit

---
+++ Brainfuck-JIT/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Brainfuck_JIT;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string program = File.ReadAllText("F:\\Desktop HDD\\repos\\Brainfuck-JIT\\Brainfuck-JIT\\hello_world.bf");
+        Console.WriteLine("loaded program");
+        JIT.Run(program);
+    }
+}
