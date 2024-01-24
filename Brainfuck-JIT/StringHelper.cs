44868075b7be6b872abfbf1c6aa236d7da609afd
Flojomojo <35773711+Flojomojo@users.noreply.github.com>
2024-01-24 21:07:15 +0100

    added StringHelper.cs

---
+++ Brainfuck-JIT/StringHelper.cs
@@ -0,0 +1,11 @@
+namespace Brainfuck_JIT;
+
+public static class StringHelper
+{
+    public static string RemoveWhitespace(string input)
+    {
+        return new string(input.ToCharArray()
+            .Where(c => !Char.IsWhiteSpace(c))
+            .ToArray());
+    } 
+}
