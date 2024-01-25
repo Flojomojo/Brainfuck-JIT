f85ae6324ad5af60613e95d4d0004c711933b27d
Flojomojo <35773711+Flojomojo@users.noreply.github.com>
2024-01-25 02:06:44 +0100

    added simple lexer

--- Brainfuck-JIT/JIT.cs
+++ Brainfuck-JIT/JIT.cs
@@ -1,10 +1,13 @@
+using System.Data;
+using System.Diagnostics;
 using System.Globalization;
 using System.IO.Enumeration;
 using System.Net.Mime;
+using System.Reflection.Metadata;
 
 namespace Brainfuck_JIT;
 
-public enum OpCode
+public enum OpCodeType
 {
     INC = '+',
     DEC = '-',
@@ -16,20 +19,22 @@ public enum OpCode
     JNZ = ']'
 }
 
+public record OpCode(OpCodeType Type, int Repetition);
+
 class BrainfuckProgram
 {
     /// <summary>
     /// The program consists of x cells each containing one single byte
     /// </summary>
-    public byte[] Memory = new byte[32768];
+    private byte[] Memory = new byte[32768];
     /// <summary>
     /// The instructions to execute
     /// </summary>
-    private List<OpCode> Instructions = [];
+    private readonly List<OpCode> _instructions = [];
     /// <summary>
     /// The pointer to the cell
     /// </summary>
-    private int DataPointer = 0;
+    private int _dataPointer = 0;
 
     /// <summary>
     /// Dumps the memory of the program and prints it to the console
@@ -49,7 +54,7 @@ class BrainfuckProgram
     private void Reset()
     {
         Array.Clear(this.Memory, 0, this.Memory.Length);
-        this.DataPointer = 0;
+        this._dataPointer = 0;
     }
 
     /// <summary>
@@ -58,7 +63,7 @@ class BrainfuckProgram
     /// <returns>The current byte</returns>
     private byte GetCurrentByte()
     {
-        return this.Memory[this.DataPointer];
+        return this.Memory[this._dataPointer];
     }
 
     /// <summary>
@@ -67,7 +72,7 @@ class BrainfuckProgram
     /// <param name="value">The byte value to set</param>
     private void SetCurrentByte(byte value)
     {
-        this.Memory[this.DataPointer] = value;
+        this.Memory[this._dataPointer] = value;
     }
 
     /// <summary>
@@ -79,46 +84,50 @@ class BrainfuckProgram
         Reset();
         int lastJumpPosition = -1;
         // Iterate over all instructions and execute them line by line
-        for (int i = 0; i < this.Instructions.Count; i++)
+        for (int i = 0; i < this._instructions.Count; i++)
         {
-            OpCode opcode = this.Instructions[i];
+            var (opCodeType, repetition) = this._instructions[i];
             byte value = GetCurrentByte();
             int intValue = value;
-            switch (opcode)
+            switch (opCodeType)
             { 
-                case OpCode.INC:
-                    // Get the byte at the current position and increase by one
-                    SetCurrentByte((byte)(intValue + 1)); 
+                case OpCodeType.INC:
+                    // Get the byte at the current position and increase by x
+                    SetCurrentByte((byte)(intValue + repetition)); 
                     break;
-                case OpCode.DEC:
-                    // Get the byte at the current position and dec by one
-                    SetCurrentByte((byte)(intValue - 1)); 
+                case OpCodeType.DEC:
+                    // Get the byte at the current position and dec by x
+                    SetCurrentByte((byte)(intValue - repetition)); 
                     break;
-                case OpCode.INCDP:
-                    // Increase the DP by one
+                case OpCodeType.INCDP:
+                    // Increase the DP by x
+                    int newDataPointer = this._dataPointer + repetition;
                     // Prevents OOB READ/WRITE
-                    if (this.DataPointer == this.Memory.Length - 1)
+                    if (newDataPointer > this.Memory.Length - 1)
                     {
-                        Console.WriteLine($"OOB Memory at {this.DataPointer + 1}");
+                        Console.WriteLine($"OOB Memory at {newDataPointer}");
                         Environment.Exit(1);
                     }
-                    this.DataPointer++;
+
+                    this._dataPointer = newDataPointer;
                     break;
-                case OpCode.DECDP:
+                case OpCodeType.DECDP:
                     // Decrease the DP by one
+                    newDataPointer = this._dataPointer - repetition;
                     // Prevents OOB READ/WRITE
-                    if (this.DataPointer == 0)
+                    if (newDataPointer < 0)
                     {
-                        Console.WriteLine($"OOB Memory at {this.DataPointer - 1}");
+                        Console.WriteLine($"OOB Memory at {newDataPointer}");
                         Environment.Exit(1);
                     }
-                    this.DataPointer--;
+
+                    this._dataPointer = newDataPointer;
                     break;
-                case OpCode.OUT:
+                case OpCodeType.OUT:
                     // Print out the current byte as a ASCII char
                     Console.Write(Convert.ToChar(value));
                     break;
-                case OpCode.INP:
+                case OpCodeType.INP:
                     // Takes a single byte as input and stores it at the current cell
                     Console.Write("> ");
                     ConsoleKeyInfo keyInfo = Console.ReadKey();
@@ -127,7 +136,7 @@ class BrainfuckProgram
                     byte byteValue = Convert.ToByte(inputChar);
                     SetCurrentByte(byteValue); 
                     break;
-                case OpCode.JZ:
+                case OpCodeType.JZ:
                     // if the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ] command.
                     if (intValue == 0)
                     {
@@ -142,7 +151,7 @@ class BrainfuckProgram
                     }
                     lastJumpPosition = i;
                     break;
-                case OpCode.JNZ:
+                case OpCodeType.JNZ:
                     // if the byte at the data pointer is non-zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ] command.
                     if (intValue != 0)
                     {
@@ -171,11 +180,46 @@ class BrainfuckProgram
     /// <param name="opcode">The opcode to append</param>
     public void AppendOpcode(OpCode opcode)
     {
-       this.Instructions.Add(opcode); 
+       this._instructions.Add(opcode); 
     }
 }
 
-class JIT
+public class Lexer(string programString)
+{
+    private int _i = 0;
+    private string _programString = programString;
+
+    public char? Peek()
+    {
+        int next = this.FindNextValidOpcode();
+        if (next == -1)
+            return null;
+        return this._programString[next];
+    }
+
+    public char Consume()
+    {
+        int next = this.FindNextValidOpcode();
+        char c = this._programString[next];
+        this._i = next;
+        return c;
+    }
+    private int FindNextValidOpcode()
+    {
+        int index = this._i + 1;
+        char c = this._programString[index];
+        while (!JIT.IsValidOpCode(c))
+        {
+            index++;
+            if (index > this._programString.Length - 1)
+                return -1;
+            c = this._programString[index];
+        }
+        return index;
+    }
+}
+
+public class JIT
 {
     /// <summary>
     /// Runs a brainfuck program
@@ -190,6 +234,11 @@ class JIT
         //program.DumpMemory();
     }
 
+    public static bool IsValidOpCode(char c)
+    {
+        return Enum.IsDefined(typeof(OpCodeType), (int)c);
+    }
+    
     /// <summary>
     /// Takes a program string and then parses it into a program
     /// </summary>
@@ -198,15 +247,23 @@ class JIT
     private static BrainfuckProgram Parse(string programString)
     {
         BrainfuckProgram program = new();
-        foreach (char opcode in programString)
+        Lexer lexer = new(programString);
+        while(lexer.Peek() is not null)
         {
-            // Check if the char is actually a valid opcode
-            if (!Enum.IsDefined(typeof(OpCode), (int)opcode))
-            {
-                // Invalid chars are just ignored
+            char opcode = lexer.Consume();
+            // Invalid opcodes are just ignored
+            if (!IsValidOpCode(opcode))
                 continue;
+            // Check if the char is actually a valid opcode
+            OpCodeType parsedOpCodeType = (OpCodeType)opcode;
+            // If there are repeating characters count them
+            int repeatCount = 1;
+            while (lexer.Peek() == opcode)
+            {
+               repeatCount++;
+               lexer.Consume();
             }
-            OpCode parsedOpCode = (OpCode)opcode;
+            OpCode parsedOpCode = new(parsedOpCodeType, repeatCount);
             program.AppendOpcode(parsedOpCode);
         }
         return program;
