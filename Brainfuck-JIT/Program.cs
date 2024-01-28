059d54941b0beef394859834589873303c2d8de4
Flojomojo <35773711+Flojomojo@users.noreply.github.com>
2024-01-28 20:04:26 +0100

    added better cmdline arg parsing

--- Brainfuck-JIT/Program.cs
+++ Brainfuck-JIT/Program.cs
@@ -1,31 +1,51 @@
 ﻿using System.Runtime.InteropServices;
 using Brainfuck_JIT.JIT;
+using CommandLine;
 
 namespace Brainfuck_JIT;
 
 class Program
 {
+
+    /// <summary>
+    /// Command Line Arguments
+    /// </summary>
+    public class CommandLineArgs
+    {
+        [Value(0, MetaName = "bf_filename", Required = true, HelpText = "Filename of the bf program.")]
+        public string? BfFilename { get; set; } 
+        [Option('i', "interpret", Default=false, HelpText = "Instead of jitting the file, interpret it.")]
+        public bool Interpret { get; set; }
+    }
     public static void Main(string[] args)
     {
-        if (args.Length < 1)
-        {
-            Console.WriteLine("Not enough command line args");
-            Console.WriteLine("Usage:");
-            Console.WriteLine("\t dotnet run {brainfuck_program.bf}");
-            Environment.Exit(1);
-        }
-        string bfFilename = args[0];
-        string outputFilename = bfFilename.Split(".")[0];
-        Console.WriteLine(outputFilename);
-        string programString = LoadProgram(bfFilename);
-        Console.WriteLine($"Loaded program {bfFilename}");
-        BrainfuckProgram program = IR.Parse(programString);
-        //Interpreter interpreter = new();
-        //interpreter.Run(program); 
-        JIT.JIT jit = new();
-        jit.Compile(program, outputFilename);
+        CommandLine.Parser.Default.ParseArguments<CommandLineArgs>(args)
+            .WithParsed(Run);
     }
 
+    /// <summary>
+    /// Actually run the program
+    /// </summary>
+    /// <param name="args">The args from the cmdline</param>
+    public static void Run(CommandLineArgs args)
+    {
+        string bfFilename = args.BfFilename!;
+        string outputFilename = bfFilename.Split(".")[0];
+        string programString = LoadProgram(bfFilename);
+        Console.WriteLine($"Loaded program '{bfFilename}'");
+        BrainfuckProgram program = IR.Parse(programString);
+        if (args.Interpret)
+        {
+            Interpreter interpreter = new();
+            interpreter.Run(program);
+        }
+        else
+        {
+            JIT.JIT jit = new();
+            jit.Compile(program, outputFilename);
+        }
+    }
+    
     /// <summary>
     /// Loads a brainfuck program from a file
     /// </summary>
